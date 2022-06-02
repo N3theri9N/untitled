0f812b1d2473332f122f5ffda47598e7b1686ba7
KDY <nnea5215@gmail.com>
2022-06-02 18:22:45 +0900

    image upload

--- src/TWDbootcamp/express/YelpCamp/routes/campgrounds.js
+++ src/TWDbootcamp/express/YelpCamp/routes/campgrounds.js
@@ -4,6 +4,9 @@ const catchAsync = require("../utils/catchAsync");
 const ExpressError = require('../utils/ExpressError');
 const { campgroundSchema } = require("../schemas");
 const CampgroundController = require("../controllers/campgrounds");
+const multer = require('multer');
+const { storage } = require('../cloudinary/');
+const upload = multer({storage});
 
 const validateCampground = (req, res, next) => {
   const { error } = campgroundSchema.validate(req.body);
@@ -18,9 +21,11 @@ const validateCampground = (req, res, next) => {
 
 router.route('/')
     .get(catchAsync(CampgroundController.index))
-    .post(catchAsync(CampgroundController.renderNewForm))
-
-router.get('/new', CampgroundController.createCampground);
+    .post(upload.array('image'), catchAsync(CampgroundController.createCampground))
+/*    .post(upload.array('image'), (req, res) => {
+        res.send(req.body, req.files);
+    })*/
+router.get('/new', CampgroundController.renderNewForm);
 
 router.route('/:id')
     .get(catchAsync( CampgroundController.showCampground))
--- src/TWDbootcamp/express/YelpCamp/seeds/index.js
+++ src/TWDbootcamp/express/YelpCamp/seeds/index.js
@@ -2,17 +2,19 @@ const mongoose = require('mongoose');
 const Campground = require('../models/campground');
 
 
-/*
+
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewurlParser : true,
+    useUnifiedTopology : true,
+})
+
+/*
+mongoose.connect('mongodb://root:root@localhost:27017/yelp-camp?authSource=admin&authMechanism=SCRAM-SHA-1', {
     useNewurlParser : true,
     useUnifiedTopology : true,
 })
 */
 
-mongoose.connect('mongodb://root:root@localhost:27017/yelp-camp?authSource=admin&authMechanism=SCRAM-SHA-1', {
-    useNewurlParser : true,
-    useUnifiedTopology : true,
-})
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open" , ()=>{
@@ -31,7 +33,16 @@ const seedDB = async() => {
         const camp = new Campground({
             location : `${cities[random1000].city}, ${cities[random1000].state}`,
             title : `${sample(descriptors)} ${sample(places)}`,
-            image : 'https://source.unsplash.com/collection/483251',
+            images : [
+                {
+                    url : "https://res.cloudinary.com/dvi0uzzbl/image/upload/v1654160550/YelpCamp/wxp8s9z9ty05o0p95cqd.jpg",
+                    filename : "YelpCamp/wxp8s9z9ty05o0p95cqd"
+                },
+                {
+                    url : "https://res.cloudinary.com/dvi0uzzbl/image/upload/v1654160550/YelpCamp/gswuz4ouueyqqxxzo8tz.jpg",
+                    filename : "YelpCamp/gswuz4ouueyqqxxzo8tz"
+                }
+            ],
             description : 'Description',
             price : Math.floor(Math.random() * 20) + 10
         });
